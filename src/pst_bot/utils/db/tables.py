2c902ce1fe124c4bf6b263d53c22aa14e240226f
Asad Sayfiyev <bcdev@mail.ru>
2024-01-23 23:19:02 +0000

    Middle changes..

--- src/pst_bot/utils/db/tables.py
+++ src/pst_bot/utils/db/tables.py
@@ -4,7 +4,8 @@ from datetime import datetime  # noqa: TCH003
 from typing import Optional
 
 from sqlalchemy import BigInteger, ForeignKey, String, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing_extensions import Annotated  # noqa: UP035
 
 opt_str = Annotated[Optional[str], mapped_column(default=None)]  # noqa: UP007
@@ -16,7 +17,7 @@ ticket_big_field = Annotated[str, mapped_column(String(512))]
 # TODO: repr..
 
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase, AsyncAttrs):
 	"""Just declarative base class for ORM tables."""
 
 	...
@@ -29,11 +30,19 @@ class UsersTable(Base):
 
 	__tablename__ = 'users'
 
-	user_id = mapped_column(BigInteger(), primary_key=True)
+	user_id = mapped_column(BigInteger(), autoincrement=False, primary_key=True)
 	username: Mapped[Optional[str]] = mapped_column(String(32), default=None)  # min 5  # noqa: UP007
 	full_name: Mapped[opt_str] = mapped_column(String(132), default=None)
 	language_code: Mapped[opt_str] = mapped_column(String(3))
-	datetime: Mapped[Optional[datetime]] = mapped_column(default=None, server_default=func.current_timestamp())  # noqa: UP007
+	datetime: Mapped[Optional[datetime]] = mapped_column(  # noqa: UP007
+		default=None, server_default=func.current_timestamp(),
+	)
+
+	data = relationship('users_data', uselist=False, back_populates='user')
+	profile = relationship('users_profile', uselist=False, back_populates='user')
+	channels_subscriptions = relationship('users_channels_subscriptions', back_populates='user')
+
+	tickets = relationship('apps_tickets', back_populates='user')
 
 
 class UsersDataTable(Base):
@@ -41,8 +50,10 @@ class UsersDataTable(Base):
 
 	__tablename__ = 'users_data'
 
-	user_id = mapped_column(BigInteger(), ForeignKey(UsersTable.user_id, ), primary_key=True)  # TODO: Mb on user delete trigger in DB class..
+	user_id = mapped_column(BigInteger(), ForeignKey(UsersTable.user_id, ), autoincrement=False, primary_key=True)  # TODO: Mb on user delete trigger in DB class..
 	current_language_code: Mapped[str] = mapped_column(default='ru')
+
+	user = relationship('users', back_populates='data')
 
 
 class UsersChannelsSubscriptionsTable(Base):
@@ -55,6 +66,8 @@ class UsersChannelsSubscriptionsTable(Base):
 	user_id = mapped_column(BigInteger(), ForeignKey(UsersTable.user_id, ))  # TODO: Mb on user delete trigger in DB class..
 	channel_id = mapped_column(BigInteger(), nullable=False)
 
+	user = relationship('users', back_populates='channels_subscriptions')
+
 
 # TODO: Profiles table with ~options (aka options table)..
 class UsersProfileTable(Base):
@@ -63,11 +76,13 @@ class UsersProfileTable(Base):
 
 	id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
 
-	user_id: Mapped[int] = mapped_column(BigInteger(), nullable=False)
+	user_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
 
 	# TODO: Move into the other table..
 	# Lazy move..
 	gmails: Mapped[str] = mapped_column(String(), nullable=False)  # gmails comma separated..
+
+	user = relationship('users', back_populates='data')
 
 
 # TODO: From users-side optionally hide some opts..
@@ -81,8 +96,9 @@ class AppsTicketsTable(Base):
 
 	# Owner data
 	# FIXME: Foreign keys in pg -_-
-	by_dev_id = mapped_column(BigInteger(), nullable=False)#, ForeignKey(UsersTable.user_id, ))  # TODO: Mb on user delete trigger in DB class..
-	by_dev_username = mapped_column(String(32), default=None)#, ForeignKey(UsersTable.username, ))
+	by_dev_id = mapped_column(BigInteger, ForeignKey(UsersTable.user_id, ), nullable=False)  # TODO: Mb on user delete trigger in DB class..
+	#
+	by_dev_username = mapped_column(String(32), ForeignKey(UsersTable.username, ), default=None)
 
 	active_testers_count: Mapped[int] = mapped_column(default=0)
 
@@ -96,12 +112,8 @@ class AppsTicketsTable(Base):
 	# ~approximately
 	dev_response_period = mapped_column(String(32), default='-')  # TODO: Table with schedules..
 
-	# __table_args__ = (
-	# 	ForeignKeyConstraint(
-	# 		[by_dev_id, by_dev_username],
-	# 		[UsersTable.user_id, UsersTable.username],
-	# 	),
-	# )
+	user = relationship('users', back_populates='tickets')
+	testers = relationship('apps_testers_data', back_populates='ticket')
 
 
 class AppsTestersDataTable(Base):
@@ -120,3 +132,5 @@ class AppsTestersDataTable(Base):
 	# TODO: Use bools..
 	dev_status: Mapped[str] = mapped_column(String(16), default='accepted', nullable=False)
 	tester_status: Mapped[str] = mapped_column(String(16), default='wait', nullable=False)
+
+	tester = relationship('users')
